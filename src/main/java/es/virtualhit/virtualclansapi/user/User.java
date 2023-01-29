d895f0fccfb9987cdbf391db9dcdb7fd93fbf563
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-29 23:49:15 +0100

    Update user object

--- src/main/java/es/virtualhit/virtualclansapi/user/User.java
+++ src/main/java/es/virtualhit/virtualclansapi/user/User.java
@@ -1,15 +1,23 @@
 package es.virtualhit.virtualclansapi.user;
 
+import es.virtualhit.virtualclansapi.user.chat.ChatStatus;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.UUID;
 
 public abstract class User {
 
     private final UUID uuid;
     private UUID clanUUID;
+    private LocalDateTime lastDonation;
+    private ChatStatus chatStatus;
 
     public User(UUID uuid) {
         this.uuid = uuid;
         this.clanUUID = null;
+        this.lastDonation = LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC);
+        this.chatStatus = ChatStatus.GLOBAL;
     }
 
     public UUID getUuid() {
@@ -22,5 +30,21 @@ public abstract class User {
 
     public void setClanUUID(UUID clanUUID) {
         this.clanUUID = clanUUID;
+    }
+
+    public LocalDateTime getLastDonation() {
+        return lastDonation;
+    }
+
+    public void setLastDonation(LocalDateTime lastDonation) {
+        this.lastDonation = lastDonation;
+    }
+
+    public ChatStatus getChatStatus() {
+        return chatStatus;
+    }
+
+    public void setChatStatus(ChatStatus chatStatus) {
+        this.chatStatus = chatStatus;
     }
 }
