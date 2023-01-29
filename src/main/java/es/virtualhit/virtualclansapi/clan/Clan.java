3a18377d4480a11c4a99fc9bfa162d3b1865c88e
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-29 23:29:50 +0100

    Update clan object

--- src/main/java/es/virtualhit/virtualclansapi/clan/Clan.java
+++ src/main/java/es/virtualhit/virtualclansapi/clan/Clan.java
@@ -2,6 +2,11 @@ package es.virtualhit.virtualclansapi.clan;
 
 import es.virtualhit.virtualclansapi.clan.rank.ClanPermission;
 import es.virtualhit.virtualclansapi.clan.rank.ClanRank;
+import net.william278.huskhomes.position.Position;
+import org.bukkit.block.Banner;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,20 +17,32 @@ public abstract class Clan {
 
     private final UUID uuid;
     private String name;
+    private String description;
+    private ItemStack banner;
     private UUID leader;
     private List<UUID> members;
     private List<UUID> onlineMembers;
     private int experience;
     private HashMap<String, ClanRank> ranks;
+    private List<UUID> allies;
+    private HashMap<Integer, Inventory> chests;
+    private Position home;
+    private Inventory mailbox;
+
+    // TODO: Quests
 
     public Clan(UUID uuid, String name, UUID leader) {
         this.uuid = uuid;
         this.name = name;
         this.leader = leader;
+
+        this.description = "";
         this.members = new ArrayList<>();
         this.onlineMembers = new ArrayList<>();
         this.experience = 0;
         this.ranks = new HashMap<>();
+        this.allies = new ArrayList<>();
+        this.chests = new HashMap<>();
     }
 
     public UUID getUuid() {
@@ -38,6 +55,22 @@ public abstract class Clan {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ItemStack getBanner() {
+        return banner;
+    }
+
+    public void setBanner(ItemStack banner) {
+        this.banner = banner;
     }
 
     public UUID getLeader() {
@@ -80,7 +113,40 @@ public abstract class Clan {
         this.ranks = ranks;
     }
 
+    public List<UUID> getAllies() {
+        return allies;
+    }
+
+    public void setAllies(List<UUID> allies) {
+        this.allies = allies;
+    }
+
+    public HashMap<Integer, Inventory> getChests() {
+        return chests;
+    }
+
+    public void setChests(HashMap<Integer, Inventory> chests) {
+        this.chests = chests;
+    }
+
+    public Position getHome() {
+        return home;
+    }
+
+    public void setHome(Position home) {
+        this.home = home;
+    }
+
+    public Inventory getMailbox() {
+        return mailbox;
+    }
+
+    public void setMailbox(Inventory mailbox) {
+        this.mailbox = mailbox;
+    }
+
     public abstract int calculateLevel();
 
     public abstract boolean hasPermission(UUID member, ClanPermission permission);
+    public abstract void teleportHome(Player player);
 }
