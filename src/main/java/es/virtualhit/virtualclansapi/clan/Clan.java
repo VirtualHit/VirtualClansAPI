9a02cecba0e73da68efb9a59a246f1ff5c9f0848
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-29 23:02:49 +0100

    Add methods to clan object

--- src/main/java/es/virtualhit/virtualclansapi/clan/Clan.java
+++ src/main/java/es/virtualhit/virtualclansapi/clan/Clan.java
@@ -1,5 +1,6 @@
 package es.virtualhit.virtualclansapi.clan;
 
+import es.virtualhit.virtualclansapi.clan.rank.ClanPermission;
 import es.virtualhit.virtualclansapi.clan.rank.ClanRank;
 
 import java.util.ArrayList;
@@ -26,4 +27,60 @@ public abstract class Clan {
         this.experience = 0;
         this.ranks = new HashMap<>();
     }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getLeader() {
+        return leader;
+    }
+
+    public void setLeader(UUID leader) {
+        this.leader = leader;
+    }
+
+    public List<UUID> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<UUID> members) {
+        this.members = members;
+    }
+
+    public List<UUID> getOnlineMembers() {
+        return onlineMembers;
+    }
+
+    public void setOnlineMembers(List<UUID> onlineMembers) {
+        this.onlineMembers = onlineMembers;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public HashMap<String, ClanRank> getRanks() {
+        return ranks;
+    }
+
+    public void setRanks(HashMap<String, ClanRank> ranks) {
+        this.ranks = ranks;
+    }
+
+    public abstract int calculateLevel();
+
+    public abstract boolean hasPermission(UUID member, ClanPermission permission);
 }
