e9ceed573d4c58150bde1b9e95788148eb81a957
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-29 23:37:51 +0100

    Refactor user service

--- src/main/java/es/virtualhit/virtualclansapi/user/UserService.java
+++ src/main/java/es/virtualhit/virtualclansapi/user/UserService.java
@@ -1,11 +1,13 @@
 package es.virtualhit.virtualclansapi.user;
 
+import es.virtualhit.virtualclansapi.service.UpdateType;
+
 import java.util.UUID;
 
 public interface UserService {
 
     void create(User user);
-    void update(User user);
+    void update(User user, UpdateType updateType);
     boolean exists(UUID uuid);
     boolean exists(String name);
     User get(UUID uuid);
