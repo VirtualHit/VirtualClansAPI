50da60a6b5c9ba509ff5afdc984e005eeb06ff1f
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-30 19:58:15 +0100

    Update constructor

--- src/main/java/es/virtualhit/virtualclansapi/clan/rank/ClanRank.java
+++ src/main/java/es/virtualhit/virtualclansapi/clan/rank/ClanRank.java
@@ -1,5 +1,6 @@
 package es.virtualhit.virtualclansapi.clan.rank;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +15,8 @@ public abstract class ClanRank {
     public ClanRank(String name, int weight, boolean nameChangeable, boolean weightChangeable) {
         this.name = name;
         this.weight = weight;
+        this.permissions = new ArrayList<>();
+        this.members = new ArrayList<>();
         this.nameChangeable = nameChangeable;
         this.weightChangeable = weightChangeable;
     }
