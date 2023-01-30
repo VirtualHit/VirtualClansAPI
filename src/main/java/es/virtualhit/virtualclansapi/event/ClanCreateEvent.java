1f121683ee7cf5bc2aeafbd03adeb1a5bca7d4ad
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-30 12:58:38 +0100

    Add clan create event

---
+++ src/main/java/es/virtualhit/virtualclansapi/event/ClanCreateEvent.java
@@ -0,0 +1,31 @@
+package es.virtualhit.virtualclansapi.event;
+
+import es.virtualhit.virtualclansapi.clan.Clan;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class ClanCreateEvent extends PlayerEvent {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+    private final Clan clan;
+
+    public ClanCreateEvent(@NotNull Player who, @NotNull Clan clan) {
+        super(who);
+        this.clan = clan;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    public @NotNull Clan getClan() {
+        return clan;
+    }
+}
