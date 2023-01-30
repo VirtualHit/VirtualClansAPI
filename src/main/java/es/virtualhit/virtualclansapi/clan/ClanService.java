b2eafc022df18fa43c44beec5a8bf35aaa0d7c81
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-30 12:30:11 +0100

    Add another method to clan service

--- src/main/java/es/virtualhit/virtualclansapi/clan/ClanService.java
+++ src/main/java/es/virtualhit/virtualclansapi/clan/ClanService.java
@@ -1,18 +1,22 @@
 package es.virtualhit.virtualclansapi.clan;
 
 import es.virtualhit.virtualclansapi.service.UpdateType;
-import es.virtualhit.virtualclansapi.user.User;
 
 import java.util.UUID;
 
 public interface ClanService {
 
     void create(Clan clan);
+
     void update(Clan clan, UpdateType updateType);
+
     boolean exists(UUID uuid);
+
     Clan get(UUID uuid);
+
     void loadToRedis(Clan clan);
+
     void unloadFromRedis(UUID uuid);
 
-
+    boolean isLoadedInRedis(UUID uuid);
 }
