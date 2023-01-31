4dd81367ea2c2faf7347c5e475a2f6d45a74b759
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-01-31 12:52:41 +0100

    Add clan uuid to invite

--- src/main/java/es/virtualhit/virtualclansapi/invite/Invite.java
+++ src/main/java/es/virtualhit/virtualclansapi/invite/Invite.java
@@ -1,15 +1,22 @@
 package es.virtualhit.virtualclansapi.invite;
 
 import java.util.Locale;
+import java.util.UUID;
 
 public abstract class Invite {
     private final String id;
+    private final UUID clanUUID;
 
-    public Invite(String senderName, String receiverName) {
+    public Invite(String senderName, String receiverName, UUID clanUUID) {
         this.id = senderName.toLowerCase(Locale.ROOT) + receiverName.toLowerCase(Locale.ROOT);
+        this.clanUUID = clanUUID;
     }
 
     public String getId() {
         return id;
+    }
+
+    public UUID getClanUUID() {
+        return clanUUID;
     }
 }
