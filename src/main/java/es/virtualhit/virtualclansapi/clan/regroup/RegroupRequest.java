05eaabd3e9313068d73cb29bd460113ffd80163b
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-02-02 11:39:25 +0100

    Ya decia yo que me faltaba algo

--- src/main/java/es/virtualhit/virtualclansapi/clan/regroup/RegroupRequest.java
+++ src/main/java/es/virtualhit/virtualclansapi/clan/regroup/RegroupRequest.java
@@ -1,16 +1,30 @@
 package es.virtualhit.virtualclansapi.clan.regroup;
 
+import net.william278.huskhomes.position.Position;
+
 import java.util.UUID;
 
 public abstract class RegroupRequest {
 
     private final UUID clanUUID;
+    private final Position position;
+    private final UUID senderUUID;
 
-    public RegroupRequest(UUID clanUUID) {
+    public RegroupRequest(UUID clanUUID, Position position, UUID senderUUID) {
         this.clanUUID = clanUUID;
+        this.position = position;
+        this.senderUUID = senderUUID;
     }
 
     public UUID getClanUUID() {
         return clanUUID;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public UUID getSenderUUID() {
+        return senderUUID;
     }
 }
