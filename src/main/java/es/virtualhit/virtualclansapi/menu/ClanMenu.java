a7f6da069767a320d38f190932a92db0e2440c5b
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-02-04 15:37:59 +0100

    Update clan menu

--- src/main/java/es/virtualhit/virtualclansapi/menu/ClanMenu.java
+++ src/main/java/es/virtualhit/virtualclansapi/menu/ClanMenu.java
@@ -1,11 +1,8 @@
 package es.virtualhit.virtualclansapi.menu;
 
-import es.virtualhit.virtualmenu.event.PlayerClickMenuItemEvent;
 import org.bukkit.entity.Player;
 
 public abstract class ClanMenu {
 
     public abstract void open(Player player);
-
-    public abstract void executeActions(PlayerClickMenuItemEvent event);
 }
