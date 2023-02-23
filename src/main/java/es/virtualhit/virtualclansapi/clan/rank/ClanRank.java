c304bb38085ebf10c22607d451372b1707400b7a
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-02-23 13:04:57 +0100

    Fix

--- src/main/java/es/virtualhit/virtualclansapi/clan/rank/ClanRank.java
+++ src/main/java/es/virtualhit/virtualclansapi/clan/rank/ClanRank.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public abstract class ClanRank {
+public class ClanRank {
     private String name;
     private int weight;
     private List<ClanPermission> permissions;
