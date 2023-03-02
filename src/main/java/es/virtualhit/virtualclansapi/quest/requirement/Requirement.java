99792c7f8fa95bab151aecbe148b5aa54d2a5708
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-03-02 12:53:17 +0100

    Add clan complete quest event

--- src/main/java/es/virtualhit/virtualclansapi/quest/requirement/Requirement.java
+++ src/main/java/es/virtualhit/virtualclansapi/quest/requirement/Requirement.java
@@ -3,5 +3,30 @@ package es.virtualhit.virtualclansapi.quest.requirement;
 public class Requirement {
 
     private RequirementType type;
+    private int current;
     private int amount;
+
+    public RequirementType getType() {
+        return type;
+    }
+
+    public void setType(RequirementType type) {
+        this.type = type;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(int current) {
+        this.current = current;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
