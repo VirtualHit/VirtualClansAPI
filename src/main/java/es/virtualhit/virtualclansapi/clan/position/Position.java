4ea0f99b6b7d524809d75a3fecad644b1d65bc86
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-07-14 23:45:01 +0200

    Fix

--- src/main/java/es/virtualhit/virtualclansapi/clan/position/Position.java
+++ src/main/java/es/virtualhit/virtualclansapi/clan/position/Position.java
@@ -2,18 +2,15 @@ package es.virtualhit.virtualclansapi.clan.position;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Optional;
-import java.util.UUID;
-
 public class Position extends Location {
-    public Server server;
+    public String server;
 
-    public Position(double x, double y, double z, float yaw, float pitch, @NotNull World world, @NotNull Server server) {
+    public Position(double x, double y, double z, float yaw, float pitch, @NotNull World world, @NotNull String server) {
         super(x, y, z, yaw, pitch, world);
         this.server = server;
     }
 
-    public Position(@NotNull Location location, @NotNull Server server) {
+    public Position(@NotNull Location location, @NotNull String server) {
         super(location.x, location.y, location.z, location.yaw, location.pitch, location.world);
         this.server = server;
     }
@@ -29,30 +26,5 @@ public class Position extends Location {
         this.pitch = newPosition.pitch;
         this.world = newPosition.world;
         this.server = newPosition.server;
-    }
-
-    public static Optional<Position> parse(@NotNull String[] args, @NotNull Position relativeTo) {
-        if (args.length >= 3 && args.length <= 5) {
-            try {
-                double x = parseCoordinate(args[0], relativeTo.x);
-                double y = parseCoordinate(args[1], relativeTo.y);
-                double z = parseCoordinate(args[2], relativeTo.z);
-                World world = args.length > 3 ? new World(args[3], UUID.randomUUID()) : relativeTo.world;
-                Server server = args.length > 4 ? new Server(args[4]) : relativeTo.server;
-                return Optional.of(new Position(x, y, z, relativeTo.yaw, relativeTo.pitch, world, server));
-            } catch (NumberFormatException var10) {
-                return Optional.empty();
-            }
-        } else {
-            return Optional.empty();
-        }
-    }
-
-    private static double parseCoordinate(@NotNull String value, double currentValue) throws NumberFormatException {
-        if (value.startsWith("~")) {
-            return value.length() == 1 ? currentValue : currentValue + Double.parseDouble(value.substring(1));
-        } else {
-            return Double.parseDouble(value);
-        }
     }
 }
