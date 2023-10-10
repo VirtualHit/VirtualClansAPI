feccaaa9f609a4b0d78853d0b42df040c2695441
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-10-10 10:35:00 +0200

    Add QuestRequierementType

---
+++ src/main/java/es/virtualhit/virtualclansapi/clan/quest/requirement/QuestRequirementType.java
@@ -0,0 +1,16 @@
+package es.virtualhit.virtualclansapi.clan.quest.requirement;
+
+public enum QuestRequirementType {
+    GIVE,
+    BREAK,
+    PLACE,
+    COW_MILK,
+    ENCHANT,
+    FISH,
+    COOK,
+    SHEAR,
+    CRAFT,
+    KILL,
+    TAME,
+    EAT
+}
